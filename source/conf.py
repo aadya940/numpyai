394f3f466c86985a6d52ba017cbd94827ce038d7
Aadya Chinubhai <aadyachinubhai@gmail.com>
2025-04-04 23:23:01 +0530

    add readthedocs

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
